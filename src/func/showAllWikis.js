e60633dc4cc717f01cd067dff898bc2a020b2168
Nixeff <theodore.folkesson@gmail.com>
2022-11-29 15:08:37 +0100

    ,code

--- src/func/showAllWikis.js
+++ src/func/showAllWikis.js
@@ -1,22 +1,33 @@
 import { useEffect, useState } from "react";
 
-function showAllWikis(){
-    const [wikis, setWikis] = useState();
+function ShowAllWikis(){
+    const [wikis, setWikis] = useState([{"ID":2,"Type":"wiki","Title":"asd"},]);
     const getWikis = async () => {
         let API_URL = "https://acesoft.ntigskovde.se/Ace-Software/search.php?type=wiki";
         const response = await fetch(`${API_URL}`);
         const data = await response.json();
-        setWikis(data.data);
+        setWikis(data.Data); 
     }
+    
     useEffect(() => {
-        //getWikis();
+        getWikis();
     });
 
+    
+    
     return(
-        <div className={wikis} >
-        
+        <div>
+            {wikis.map( (wikis,index)=>
+                (
+                    <div key={index}>
+                        <h3>{wikis.Title}</h3>
+                        <p>{wikis.Type}</p>
+                    </div>
+                )
+                )}
         </div>
     )
 }
 
-export default showAllWikis;
+export default ShowAllWikis;
+
--- src/index.js
+++ src/index.js
@@ -1,13 +1,33 @@
-import ReactDOM from "react-dom";
-
 import App from "./App";
-import showAllWikis from "./func/showAllWikis";
+import ShowAllWikis from "./func/showAllWikis";
+import ReactDOM from "react-dom/client";
 
-const rootElement = document.getElementById("root");
-ReactDOM.render(
-  
-    <showAllWikis />
-  ,
-  rootElement
+const rootElement = ReactDOM.createRoot(document.getElementById("root"));
+rootElement.render(
+    <ShowAllWikis/>
 );
+
+
+
+//import { useState, useEffect } from "react";
+//import ReactDOM from "react-dom/client";
+
+//function Timer() {
+//  const [count, setCount] = useState(0);
+
+//  useEffect(() => {
+//    setTimeout(() => {
+//      setCount((count) => count + 1);
+//    }, 1000);
+//  });
+
+//  return <h1>I have rendered {count} times!</h1>;
+//}
+
+//const root = ReactDOM.createRoot(document.getElementById('root'));
+//root.render(<Timer />);
+
+              
+
+
 
