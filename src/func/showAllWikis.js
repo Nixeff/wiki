614300814c6c62fdec91ee6e75812eb1c471a26f
Nixeff <theodore.folkesson@gmail.com>
2022-11-30 14:05:54 +0100

    DET FUNKAR

--- src/func/showAllWikis.js
+++ src/func/showAllWikis.js
@@ -5,39 +5,36 @@ export default class ShowAllWikis extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            wikis: [],
+            wikis: "test",
+            test: "",
         }
         
     }
 
     getWikis = async () => {
         let API_URL = "https://acesoft.ntigskovde.se/Ace-Software/search.php?type=wiki";
-        const response = await fetch(`${API_URL}`);
-        const data = await response.json();
-        this.setState({
-            wikis: data.Data
+        fetch(`${API_URL}`)
+        .then((data) => data.json())
+        .then(data => {
+            this.setState({wikis: data.Data[0].Title});
         })
+        //alert(data.Data)
+        .then(
+            console.log(this.state.wikis)
+        );
     }
-
-    useEffect(
-        () => {
-            if (times % 3 === 0) {
-                setCounter(counter + 1);
-            }
-        },
-        [times]  // <--- THIS RIGHT HERE IS THE KEY!
-    );
 
     render(){
         return(
             <div>       
-                <input type="button" onClick='this.getWikis()' value="klcik"></input>
-                {this.state.wikis.map( (wikis,index)=>
+                <input type="button" onClick={() => this.getWikis()} value="klcik"></input>
+                <div>{this.state.wikis}</div>
+                {/* {this.state.wikis.map( (wikis,index)=>
                     (
                         <div key={index}>
                             <WikiTag title={wikis.Title} Type={wikis.Type} wID={wikis.ID}/>
                         </div>
-                    ))}
+                    ))} */}
             </div>
         )  
     }
