4613e3e656e2c6200ae30658b5bb8b48ee7b7902
Nixeff <theodore.folkesson@gmail.com>
2022-11-30 09:21:20 +0100

    nu klass

--- src/func/showAllWikis.js
+++ src/func/showAllWikis.js
@@ -1,33 +1,38 @@
-import { useEffect, useState } from "react";
-
-function ShowAllWikis(){
-    const [wikis, setWikis] = useState([{"ID":2,"Type":"wiki","Title":"asd"},]);
-    const getWikis = async () => {
-        let API_URL = "https://acesoft.ntigskovde.se/Ace-Software/search.php?type=wiki";
-        const response = await fetch(`${API_URL}`);
-        const data = await response.json();
-        setWikis(data.Data); 
-    }
-    
-    useEffect(() => {
-        getWikis();
-    });
-
-    
-    
-    return(
-        <div>
-            {wikis.map( (wikis,index)=>
-                (
-                    <div key={index}>
-                        <h3>{wikis.Title}</h3>
-                        <p>{wikis.Type}</p>
-                    </div>
-                )
-                )}
-        </div>
-    )
-}
+import React, { useEffect, useState } from "react";
 
 export default ShowAllWikis;
 
+class ShowAllWikis extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            wikis: [{"ID":2,"Type":"wiki","Title":"asd"}],
+        }
+    }
+
+    getWikis = async () => {
+        let API_URL = "https://acesoft.ntigskovde.se/Ace-Software/search.php?type=wiki";
+        const response = await fetch(`${API_URL}`);
+        const data = await response.json();
+        this.setState({
+            wikis: data.Data
+        })
+    }
+
+    render(){
+        return(
+            <div>
+                {this.state.wikis.map( (wikis,index)=>
+                    (
+                        <div key={index}>
+                            <h3>{wikis.Title}</h3>
+                            <p>{wikis.Type}</p>
+                        </div>
+                    ))}
+            </div>
+        )
+        
+    }
+        
+
+}
