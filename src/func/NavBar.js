d0facc926b4d631e157b8db4546aa2805d4bd622
TeaForT4 <anton.lindahl2@elev.ga.ntig.se>
2022-12-12 10:34:19 +0100

    Login name+type text

--- src/func/NavBar.js
+++ src/func/NavBar.js
@@ -1,15 +1,26 @@
 import React from "react";
+import { loadLS } from "./localStorage";
 
 function NavBar({ handleLoginClick }) {
   const handleClick = () => { // Updates the state
     handleLoginClick()
   }
+  const uname = loadLS("uname");
+  const userType = loadLS("userType");
   return (
     <div className="navbar">
       <div>
         <span onClick={handleClick} className="loginicon">
-          Sign In
+          Login
         </span>
+      </div>
+      <div>
+        <p className="user-info">
+          Namn: {uname}
+        </p>
+        <p className="user-info">
+          Användartyp: {userType}
+        </p>
       </div>
     </div>
   );
